dbfca2ff3098a239708db3db3ce7796e05d85938
ItsAFeaTure-4 <saskovegor6@gmail.com>
2026-02-19 21:19:35 +0300

    init kk6k6

---
+++ src/While.java
@@ -0,0 +1,23 @@
+
+import java.util.Random;
+
+public class While {
+    public static void main(String[] y) throws InterruptedException {
+        int x = 0;
+        while (x < 10) {
+            System.out.println(x++ + " больше -1");
+        }
+        for (int i = 0; i < 15; i += 2) {
+            System.out.println("i = " + i);
+        }
+        String[] array = new String[10];
+        array[9] = "Mm";
+        array[8] = "Mm";
+        for (int p = 0; p < array.length; p++) {
+            System.out.println(array[1]);
+        }
+
+    }
+
+}
+
